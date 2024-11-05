446393282c723428a908e0077ca45c7350ecf2c2
Snowy <87611218+Snowy893@users.noreply.github.com>
2024-11-04 22:14:49 -0500

    Delete mods/Snowy893.MimiShirt directory

--- mods/Snowy893.MimiShirt/main.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-onready var Lure = get_node("/root/SulayreLure")
-
-
-func _ready():
-	Lure.add_content("Snowy893.MimiShirt","mimi_shirt","mod://Resources/Cosmetics/undershirt_graphic_tshirt_mimi.tres", [Lure.FLAGS.FREE_UNLOCK])
-
